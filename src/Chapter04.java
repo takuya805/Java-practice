b843def25d61755b7272d3c860298a75b170c7d4
takuya805 <takuya0903exile@gmail.com>
2021-03-01 01:07:28 +0900

    java practice

---
+++ src/Chapter04.java
@@ -0,0 +1,14 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String name;
+		name = "萩原拓哉";
+		System.out.println(name);
+		name = "Thank you!!";
+		System.out.println(name);
+		final int number;
+		number = 7;
+		System.out.println(number);
+	}
+
+}
---
+++ src/Chapter05.java
@@ -0,0 +1,16 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long a;
+		a = 111111111111111111L;
+		double b;
+		b = 0.0456;
+		boolean abc;
+		abc = true;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(abc);
+	}
+
+
+}
