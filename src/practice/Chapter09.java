a24c72839a92d7e97ddd8768808c0b1f6471d6dd
takuya805 <takuya0903exile@gmail.com>
2021-03-05 23:07:34 +0900

    Java-practice

--- src/practice/Chapter09.java
+++ src/practice/Chapter09.java
@@ -2,6 +2,12 @@ package practice;
 
 public class Chapter09 {
 	public static void main(String[] args) {
-		double
+		double fuelCost = 12.5;
+		double fuelAmount = 55.0;
+
+		Car car = new Car(fuelCost, fuelAmount);
+
+		car.move(20);
+		System.out.println("残量は、"+car.getFuelAmount()+ "です");
 	}
 }
