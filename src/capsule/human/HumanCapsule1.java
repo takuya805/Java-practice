1450a539ab193ca567389b623147e9eb11aac660
takuya805 <takuya0903exile@gmail.com>
2021-03-19 21:20:48 +0900

    Java-practice

---
+++ src/capsule/human/HumanCapsule1.java
@@ -0,0 +1,24 @@
+package capsule.human;
+
+public class HumanCapsule1 {
+	 private String name;
+	    private int age;
+
+	    public HumanCapsule1(String name, int age) {
+	        this.name = name;
+	        this.age = age;
+	    }
+
+	    public String getName() {
+	        return this.name;
+	    }
+
+	    public void setName(String name) {
+	        this.name = name;
+	    }
+
+	    public int getAge() {
+	        return this.age;
+	    }
+
+}
---
+++ src/capsule/human/HumanNoCapsule1.java
@@ -0,0 +1,12 @@
+package capsule.human;
+
+public class HumanNoCapsule1 {
+	public String name;
+    public int age;
+
+    public HumanNoCapsule1(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+}
