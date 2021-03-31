0827b08e80b9b26b2fdb5d1ec03f3eae7ebe8d51
takuya805 <takuya0903exile@gmail.com>
2021-03-31 23:01:39 +0900

    java-practice

---
+++ src/ArrayList.java
@@ -0,0 +1,17 @@
+/**
+ *
+ */
+
+/**
+ * @author takuya
+ *
+ */
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+// ArrayListを生成する場合
+List<String> list1 = new ArrayList<String>();
+
+// LinkedListを生成する場合
+List<Integer> list2 = new LinkedList<Integer>();
---
+++ src/HashMap.java
@@ -0,0 +1,13 @@
+
+public class HashMap {
+	import java.util.HashMap;
+	import java.util.Map;
+	import java.util.TreeMap;
+
+	// HashMapを生成する場合
+	Map<Integer, String> map1 = new HashMap<Integer, String>();
+
+	// TreeMapを生成する場合
+	Map<String, String> map2 = new TreeMap<String, String>();
+
+}
